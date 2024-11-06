ea0688c19991daf7337b2171e8bbe542a04f70fe
May-xxvi98 <shielamaydevega98@gmail.com>
2024-11-06 16:09:54 +0000

    Arithmetic

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,1 +1,24 @@
+class ArithmeticTask {
 
+    public static void main (String[] args){
+          
+        long z=1;
+        long s= (z+ 2);
+        //int result = 1 + 2; // result is now 3
+        System.out.println(s);
+
+        s = s - 1; // result is now 2
+        System.out.println(s);
+
+        s = s * 2; // result is now 4
+        System.out.println(s);
+
+       s = s / 2; // result is now 2
+        System.out.println(s);
+
+        s = s + 8; // result is now 10
+        s = s % 7; // result is now 3
+        System.out.println(s);
+
+    }
+}
